11ff16009441268479a71f4b3f4729e4095b9154
Claudio <claudio.marino@outlook.com>
2025-11-05 12:07:50 +0100

    UPDATED

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,65 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { AuthGateway, Home, useAuthStore } from '../autoBarrell'
+import { useEffect, useState } from 'react'
+import {
+	CreateProfile,
+	Home,
+	Login,
+	Register,
+	useAuthStore,
+	useUsersStore,
+} from '../autoBarrell'
 
 const Stack = createNativeStackNavigator()
 
 export function AppRouter() {
-	const { user } = useAuthStore()
+	const { user, loading } = useAuthStore()
+	const { profileByMail } = useUsersStore()
+	const [hasProfile, setHasProfile] = useState(null)
 
-	return user ?
-			<Stack.Navigator>
+	useEffect(() => {
+		const checkProfile = async () => {
+			if (!user) {
+				setHasProfile(null)
+				return
+			}
+			try {
+				await profileByMail(user.email)
+				setHasProfile(true)
+			} catch {
+				setHasProfile(false)
+			}
+		}
+
+		checkProfile()
+	}, [user])
+
+	if (loading || (user && hasProfile === null)) {
+		return null // Puedes poner aquí un spinner de carga
+	}
+
+	return (
+		<Stack.Navigator screenOptions={{ headerShown: false }}>
+			{!user ?
+				<>
+					<Stack.Screen
+						name="Login"
+						component={Login}
+					/>
+					<Stack.Screen
+						name="Register"
+						component={Register}
+					/>
+				</>
+			: !hasProfile ?
 				<Stack.Screen
+					name="CreateProfile"
+					component={CreateProfile}
+				/>
+			:	<Stack.Screen
 					name="Home"
 					component={Home}
 				/>
-			</Stack.Navigator>
-		:	<Stack.Navigator>
-				<Stack.Screen
-					name="AuthGateway"
-					component={AuthGateway}
-				/>
-			</Stack.Navigator>
+			}
+		</Stack.Navigator>
+	)
 }
