4fbc777d1e730a78feeed2716fde5ea46e82f6af
Claudio <claudio.marino@outlook.com>
2025-11-05 12:41:03 +0100

    CHANGED THE LOGOUT BUTTON

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,25 @@
+import { useNavigation } from '@react-navigation/native'
 import { Button, Text, View } from 'react-native'
 import { useAuthStore } from '../autoBarrell'
 
 export function Home() {
 	const { signOut } = useAuthStore()
+	const navigation = useNavigation()
+
+	const handleLogout = async () => {
+		try {
+			await signOut()
+			navigation.replace('Login')
+		} catch (error) {
+			console.error('Error al cerrar sesión:', error.message)
+		}
+	}
 
 	return (
 		<View className="flex-1 justify-center items-center bg-blue-300">
 			<Button
 				title="Cerrar sesión"
-				onPress={signOut}
+				onPress={handleLogout}
 			/>
 			<Text className="text-7xl text-yellow-600 font-bold">Home</Text>
 		</View>
