2832b42f30735b49827c74c7a6a69bdfe67dc6cd
Claudio <claudio.marino@outlook.com>
2025-11-11 16:03:41 +0100

    UPDATED UI

--- src/components/organismos/CardContainer.jsx
+++ src/components/organismos/CardContainer.jsx
@@ -2,18 +2,42 @@ import { LinearGradient } from 'expo-linear-gradient'
 import { StyleSheet, View } from 'react-native'
 
 export function CardContainer({ children, progressWidth = '65%' }) {
-  return (
-    <View style={styles.card}>
-      <View style={styles.progressBarContainer}>
-        <LinearGradient colors={['#4facfe', '#00f2fe', '#43e97b']} style={[styles.progressFill, { width: progressWidth }]} start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} />
-      </View>
-      {children}
-    </View>
-  )
+	return (
+		<View style={styles.card}>
+			<View style={styles.progressBarContainer}>
+				<LinearGradient
+					colors={['#4facfe', '#00f2fe', '#43e97b']}
+					style={[styles.progressFill, { width: progressWidth }]}
+					start={{ x: 0, y: 0 }}
+					end={{ x: 1, y: 0 }}
+				/>
+			</View>
+			{children}
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  card: { backgroundColor: '#fff', borderRadius: 24, padding: 16, shadowColor: '#000', shadowOffset: { width: 0, height: 10 }, shadowOpacity: 0.2, shadowRadius: 16, elevation: 6 },
-  progressBarContainer: { position: 'absolute', top: 0, left: 0, right: 0, height: 4, backgroundColor: '#E5E7EB', borderTopLeftRadius: 24, borderTopRightRadius: 24 },
-  progressFill: { height: '100%' },
+	card: {
+		backgroundColor: '#fff',
+		borderRadius: 24,
+		padding: 16,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 10 },
+		shadowOpacity: 0.2,
+		shadowRadius: 16,
+		elevation: 6,
+	},
+	progressBarContainer: {
+		position: 'absolute',
+		top: 0,
+		left: 15,
+		right: 0,
+		height: 4,
+		backgroundColor: '#E5E7EB',
+		borderTopLeftRadius: 48,
+		borderTopRightRadius: 48,
+		width: '100%',
+	},
+	progressFill: { height: '100%' },
 })
