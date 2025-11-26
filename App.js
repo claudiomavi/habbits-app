10759ea268e0444c018628cbc384f85540907d73
Claudio <claudio.marino@outlook.com>
2025-11-26 09:17:02 +0100

    UPDATED WITH NEW STYLES

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
+import { NavigationContainer } from '@react-navigation/native'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useEffect } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { NavigationContainer } from '@react-navigation/native'
 import { AppRouter, useAuthStore } from './src/autoBarrell'
 const queryClient = new QueryClient()
 
@@ -15,9 +15,16 @@ export default function App() {
 	return (
 		<SafeAreaProvider>
 			<QueryClientProvider client={queryClient}>
-				<NavigationContainer>
-					<AppRouter />
-				</NavigationContainer>
+				{(() => {
+					const FontsProvider = require('./src/styles/FontsProvider').default
+					return (
+						<FontsProvider>
+							<NavigationContainer>
+								<AppRouter />
+							</NavigationContainer>
+						</FontsProvider>
+					)
+				})()}
 			</QueryClientProvider>
 		</SafeAreaProvider>
 	)
